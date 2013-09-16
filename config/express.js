8fe2d200820ae4f8e19dcc87d5523793a69d367d
Alexander Kahoun <alexkahoun@gmail.com>
2013-09-16 08:55:31 -0700

    fixed polymer css

--- config/express.js
+++ config/express.js
@@ -3,14 +3,13 @@ var express = require('express');
 module.exports = function(app, config) {
   app.configure(function () {
     app.use(express.compress());
-    app.use(express.static(config.root + '/public'));
     app.set('port', config.port);
     app.set('views', config.root + '/app/views');
     app.set('view engine', 'jade');
     app.use(express.favicon(config.root + '/public/img/favicon.ico'));
-    app.use('/polymer-el', express.static(config.root + '/app/polymer-elements'));
-    app.use('/polymer-ui', express.static(config.root + '/app/polymer-ui-elements'));
-    app.use('/content', express.static(config.root + '/app/public'));
+    app.use(express.static(config.root + '/app/public'));
+    app.use('/polymer-elements', express.static(config.root + '/app/polymer-elements'));
+    app.use('/polymer-ui-elements', express.static(config.root + '/app/polymer-ui-elements'));
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
